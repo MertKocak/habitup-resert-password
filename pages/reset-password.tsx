e6e2f3344dec7c66ae789021d762db912a943c48
Mert Koçak <mertkocak.2811@gmail.com>
2025-02-19 14:30:10 +0300

    ui2

--- pages/reset-password.tsx
+++ pages/reset-password.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
+import '../styles/global.css';
 
 const ResetPassword = () => {
   const [password, setPassword] = useState("");
